ab0504072f561a19bac089af73cc741cde3518ec
Bazilio050 <yalik.g@gmail.com>
2026-07-29 12:13:49 +0400

    Update app.py

--- app.py
+++ app.py
@@ -55,7 +55,7 @@ if user_input := st.chat_input("Напишите маршрут и груз (н�
 3. Минимальное расстояние: Экспорт 101 км, Импорт 151 км.
 4. Выдавай расчет строго по структурированному шаблону с формулами и готовыми цифрами.
             """
-            model = genai.GenerativeModel("gemini-1.5-flash", system_instruction=system_instruction)
+            model = genai.GenerativeModel("gemini-1.5-flash-latest", system_instruction=system_instruction)
             response = model.generate_content(user_input)
             st.markdown(response.text)
             st.session_state.messages.append({"role": "assistant", "content": response.text})
