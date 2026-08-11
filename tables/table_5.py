a9b78175adf2603021e6a8046fe2d1a3347a19da
Bazilio050 <yalik.g@gmail.com>
2026-08-11 21:32:28 +0400

    Update table_5.py

--- tables/table_5.py
+++ tables/table_5.py
@@ -148,9 +148,6 @@ def get_table_5_coefficients(shipment_type_code=None, wagon_type=None, gng_code=
     Возвращает специфические коэффициенты Таблицdef get_table_5_coefficients(shipment_type_code=None, wagon_type=None, gng_code=None, 
                            ref_wagons_cnt=None, is_2tier_platform=False, 
                            is_fruit_veg_discount=False, lang="AZ", *args, **kwargs):
-    """
-    Возвращает специфические коэффициенты Таблицы 5.
-    """
     coeffs = []
     notes = []
 
