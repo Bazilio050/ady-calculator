7d1ead1bc259dd7133042734d6946dded6e44be8
Bazilio050 <yalik.g@gmail.com>
2026-08-12 16:35:32 +0400

    Update tests.py

--- tests.py
+++ tests.py
@@ -122,10 +122,10 @@ TEST_SUITE = [
         "expected_rate": 26.66
     },
     {
-        "name": "13. Баку-тов -> Ялама (4407, крытый, 50т, СПС)",
-        "raw_text": "Баку-тов Ялама 4407 крытый 50тн СПС",
+        "name": "13. Баку тов -> Ялама (4407, крытый, 50т, СПС)",
+        "raw_text": "Баку тов Ялама 4407 крытый 50тн СПС",
         "nlu": {
-            "route_from": "Bakı-Yük (547105)", "route_to": "Yalama", "cargo_gng_code": "4407", "cargo_name": "Taxta",
+            "route_from": "Bakı Yük (547105)", "route_to": "Yalama", "cargo_gng_code": "4407", "cargo_name": "Taxta",
             "actual_weight_tons": 50.0, "wagon_type": "universal", "park_type": "SPS", "explicit_mode": "export"
         },
         "expected_rate": 15.38
