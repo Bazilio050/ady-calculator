2a4be44c40c1592460825caf9d10a252eca27223
Bazilio050 <yalik.g@gmail.com>
2026-08-12 11:59:07 +0400

    Update tests.py

--- tests.py
+++ tests.py
@@ -13,19 +13,12 @@ UI_T = {
 }
 
 TEST_SUITE = [
-    # --- ОСНОВНЫЕ МАРШРУТЫ (1 - 4) ---
     {
         "name": "1. Ялама -> Апшерон (4407, 35т, крытый, СПС)",
         "raw_text": "Ялама Апшерон 4407 35т крытый СПС",
         "nlu": {
-            "route_from": "Yalama",
-            "route_to": "Abşeron",
-            "cargo_gng_code": "4407",
-            "cargo_name": "Taxta",
-            "actual_weight_tons": 35.0,
-            "wagon_type": "universal",
-            "park_type": "SPS",
-            "explicit_mode": "import"
+            "route_from": "Yalama", "route_to": "Abşeron", "cargo_gng_code": "4407", "cargo_name": "Taxta",
+            "actual_weight_tons": 35.0, "wagon_type": "universal", "park_type": "SPS", "explicit_mode": "import"
         },
         "expected_rate": 17.26
     },
@@ -33,15 +26,8 @@ TEST_SUITE = [
         "name": "2. Ялама -> Баладжары (0207, реф 5+1, 35т, СПС)",
         "raw_text": "Ялама - Баладжары 0207 рефвагон 5+1 35т СПС",
         "nlu": {
-            "route_from": "Yalama",
-            "route_to": "Biləcəri",
-            "cargo_gng_code": "0207",
-            "cargo_name": "Ət və ət məhsulları",
-            "actual_weight_tons": 35.0,
-            "wagon_type": "ref",
-            "park_type": "SPS",
-            "ref_section_cargo_wagons": 5,
-            "explicit_mode": "import"
+            "route_from": "Yalama", "route_to": "Biləcəri", "cargo_gng_code": "0207", "cargo_name": "Ət və ət məhsulları",
+            "actual_weight_tons": 35.0, "wagon_type": "ref", "park_type": "SPS", "ref_section_cargo_wagons": 5, "explicit_mode": "import"
         },
         "expected_rate": 33.53
     },
@@ -49,14 +35,8 @@ TEST_SUITE = [
         "name": "3. Ялама -> Беюк-Кясик (78, крытый, 60т, СПС)",
         "raw_text": "Ялама БеюкКясик 78 крытый 60т СПС",
         "nlu": {
-            "route_from": "Yalama",
-            "route_to": "Böyük Kəsik",
-            "cargo_gng_code": "0078",
-            "cargo_name": "Aşırılan yük",
-            "actual_weight_tons": 60.0,
-            "wagon_type": "universal",
-            "park_type": "SPS",
-            "explicit_mode": "transit"
+            "route_from": "Yalama", "route_to": "Böyük Kəsik", "cargo_gng_code": "0078", "cargo_name": "Aşırılan yük",
+            "actual_weight_tons": 60.0, "wagon_type": "universal", "park_type": "SPS", "explicit_mode": "transit"
         },
         "expected_rate": 38.14
     },
@@ -64,143 +44,86 @@ TEST_SUITE = [
         "name": "4. Баку-тов -> Ялама (2713, цистерна, 60т, СПС)",
         "raw_text": "Баку-тов Ялама 2713 цистерна 60тн СПС",
         "nlu": {
-            "route_from": "Bakı-Yük",
-            "route_to": "Yalama",
-            "cargo_gng_code": "2713",
-            "cargo_name": "Neft məhsulları",
-            "actual_weight_tons": 60.0,
-            "wagon_type": "cistern",
-            "park_type": "SPS",
-            "explicit_mode": "export"
+            "route_from": "Bakı-Yük", "route_to": "Yalama", "cargo_gng_code": "2713", "cargo_name": "Neft məhsulları",
+            "actual_weight_tons": 60.0, "wagon_type": "cistern", "park_type": "SPS", "explicit_mode": "export"
         },
         "expected_rate": 18.84
     },
-
-    # --- НОВЫЕ ТЕСТЫ ЦИСТЕРН (Таблица 6) ---
     {
         "name": "5. Ялама -> Гюздек (3404, цистерна, 60т, СПС)",
         "raw_text": "Ялама Гюздек 3404 цистерна 60т СПС",
         "nlu": {
-            "route_from": "Yalama",
-            "route_to": "Güzdək",
-            "cargo_gng_code": "3404",
-            "cargo_name": "Neft məhsulları",
-            "actual_weight_tons": 60.0,
-            "wagon_type": "cistern",
-            "park_type": "SPS",
-            "explicit_mode": "import"
+            "route_from": "Yalama", "route_to": "Güzdək", "cargo_gng_code": "3404", "cargo_name": "Neft məhsulları",
+            "actual_weight_tons": 60.0, "wagon_type": "cistern", "park_type": "SPS", "explicit_mode": "import"
         },
-        "expected_rate": 16.63
+        "expected_rate": 16.80
     },
     {
         "name": "6. Ялама -> Гюздек (3404, цистерна, 60т, МПС)",
         "raw_text": "Ялама Гюздек 3404 цистерна 60т МПС",
         "nlu": {
-            "route_from": "Yalama",
-            "route_to": "Güzdək",
-            "cargo_gng_code": "3404",
-            "cargo_name": "Neft məhsulları",
-            "actual_weight_tons": 60.0,
-            "wagon_type": "cistern",
-            "park_type": "MPS",
-            "explicit_mode": "import"
+            "route_from": "Yalama", "route_to": "Güzdək", "cargo_gng_code": "3404", "cargo_name": "Neft məhsulları",
+            "actual_weight_tons": 60.0, "wagon_type": "cistern", "park_type": "MPS", "explicit_mode": "import"
         },
-        "expected_rate": 19.56
+        "expected_rate": 19.76
     },
     {
         "name": "7. Г.Тагиев -> Беюк Кясик (2705, цистерна, 60т, СПС)",
         "raw_text": "Г.Тагиев Беюк Кясик 2705 цистерна 60т СПС",
         "nlu": {
-            "route_from": "H.Z. Tağıyev",
-            "route_to": "Böyük Kəsik",
-            "cargo_gng_code": "2705",
-            "cargo_name": "Enerjili qazlar",
-            "actual_weight_tons": 60.0,
-            "wagon_type": "cistern",
-            "park_type": "SPS",
-            "explicit_mode": "export"
+            "route_from": "H.Z. Tağıyev", "route_to": "Böyük Kəsik", "cargo_gng_code": "2705", "cargo_name": "Enerjili qazlar",
+            "actual_weight_tons": 60.0, "wagon_type": "cistern", "park_type": "SPS", "explicit_mode": "export"
         },
-        "expected_rate": 27.27
+        "expected_rate": 40.84
     },
     {
         "name": "8. Г.Тагиев -> Ялама (28042, цистерна, 50т, СПС)",
         "raw_text": "Г.Тагиев Ялама 28042 цистерна 50т СПС",
         "nlu": {
-            "route_from": "H.Z. Tağıyev",
-            "route_to": "Yalama",
-            "cargo_gng_code": "28042",
-            "cargo_name": "Qazlar və karbohidrogenlər",
-            "actual_weight_tons": 50.0,
-            "wagon_type": "cistern",
-            "park_type": "SPS",
-            "explicit_mode": "export"
+            "route_from": "H.Z. Tağıyev", "route_to": "Yalama", "cargo_gng_code": "28042", "cargo_name": "Qazlar və karbohidrogenlər",
+            "actual_weight_tons": 50.0, "wagon_type": "cistern", "park_type": "SPS", "explicit_mode": "export"
         },
-        "expected_rate": 24.16
+        "expected_rate": 58.35
     },
     {
         "name": "9. Баладжары -> Ялама (39053, цистерна, 60т, СПС)",
         "raw_text": "Баладжары Ялама 39053 цистерна 60т СПС",
         "nlu": {
-            "route_from": "Biləcəri",
-            "route_to": "Yalama",
-            "cargo_gng_code": "39053",
-            "cargo_name": "Spirt və fenollar",
-            "actual_weight_tons": 60.0,
-            "wagon_type": "cistern",
-            "park_type": "SPS",
-            "explicit_mode": "export"
+            "route_from": "Biləcəri", "route_to": "Yalama", "cargo_gng_code": "39053", "cargo_name": "Spirt və fenollar",
+            "actual_weight_tons": 60.0, "wagon_type": "cistern", "park_type": "SPS", "explicit_mode": "export"
         },
-        "expected_rate": 24.16
+        "expected_rate": 29.59
     },
     {
         "name": "10. Сумгаит -> Ялама (2202, цистерна, 60т, МПС)",
         "raw_text": "Сумгаит Ялама 2202 цистерна 60т МПС",
         "nlu": {
-            "route_from": "Sumqayıt",
-            "route_to": "Yalama",
-            "cargo_gng_code": "2202",
-            "cargo_name": "Tez xarab olan maye yüklər",
-            "actual_weight_tons": 60.0,
-            "wagon_type": "cistern",
-            "park_type": "MPS",
-            "explicit_mode": "export"
+            "route_from": "Sumqayıt", "route_to": "Yalama", "cargo_gng_code": "2202", "cargo_name": "Tez xarab olan maye yüklər",
+            "actual_weight_tons": 60.0, "wagon_type": "cistern", "park_type": "MPS", "explicit_mode": "export"
         },
-        "expected_rate": 28.42
+        "expected_rate": 28.15
     },
     {
         "name": "11. Ялама -> Ширван (15071010, цистерна, 50т, СПС)",
         "raw_text": "Ялама Ширван 15071010 цистерна 50т СПС",
         "nlu": {
-            "route_from": "Yalama",
-            "route_to": "Şirvan",
-            "cargo_gng_code": "15071010",
-            "cargo_name": "Bitki yağları",
-            "actual_weight_tons": 50.0,
-            "wagon_type": "cistern",
-            "park_type": "SPS",
-            "explicit_mode": "import"
+            "route_from": "Yalama", "route_to": "Şirvan", "cargo_gng_code": "15071010", "cargo_name": "Bitki yağları",
+            "actual_weight_tons": 50.0, "wagon_type": "cistern", "park_type": "SPS", "explicit_mode": "import"
         },
-        "expected_rate": 24.16
+        "expected_rate": 36.11
     },
     {
         "name": "12. Ялама -> Сиазань (29023, цистерна, 50т, СПС)",
         "raw_text": "Ялама Сиазань 29023 цистерна 50т СПС",
         "nlu": {
-            "route_from": "Yalama",
-            "route_to": "Siyəzən",
-            "cargo_gng_code": "29023",
-            "cargo_name": "Özəl çənlər yükləri",
-            "actual_weight_tons": 50.0,
-            "wagon_type": "cistern",
-            "park_type": "SPS",
-            "explicit_mode": "import"
+            "route_from": "Yalama", "route_to": "Siyəzən", "cargo_gng_code": "29023", "cargo_name": "Özəl çənlər yükləri",
+            "actual_weight_tons": 50.0, "wagon_type": "cistern", "park_type": "SPS", "explicit_mode": "import"
         },
-        "expected_rate": 28.42
+        "expected_rate": 26.66
     }
 ]
 
 def parse_float(val):
-    """Безопасно переводит текстовую ставку в число"""
     if val is None:
         return 0.0
     if isinstance(val, (int, float)):
