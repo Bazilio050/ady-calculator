efe6ff7247f9c8a54601f2baec0150f5334bdfe2
Bazilio050 <yalik.g@gmail.com>
2026-08-16 16:55:51 +0400

    Update guarded_codes.py

--- guarded_codes.py
+++ guarded_codes.py
@@ -4,50 +4,54 @@ import zlib
 
 # Полный реестр 6 866 охраняемых кодов ГНГ из oxrana.xlsx в упакованном виде
 _GUARDED_DATA_B64 = """
-eJxNnUmi5SoOBTf0B6kO0P43Vg90wrdykI6HhEQrY9xc+/fP/v39++/vfzMDWtD2gQEOtEDKjh03oAWB
-KBAFokSUiBJRISr7gQVQwBFMeYrsi+zLfmBAAAVsA/qBk8v/GgQIoIAtaAcSWMAr4QoMxl8TAQEUsAXp
-QAILwODCzsLOws7CTmOnsdPYUcFSBdu02LYfGDDNu2neg/JB+bgBGhKHKh9cnAIWMAaNcfjgNvhACfqJ
-HB33HxggHQz+gQEBFLCByRVYjpkFDwyQjgWiQBSIGlE7kEABUqbwf+AlCEnQQAsaUf+3v3861Y5/pIAD
-/YEDLdATaEHTU1O7x60G31AAYp636g9aEAgd53Xf5/wDB5SgEak3+38P9AYI9AcGtAAt5r35/rAAtID0
-BwaM59jE2I38/rEw30A093p+vI14sIAGfD43TzL+/cAAMf/EfsC4aZ409A8MkJm0ySnbBAso+Ie2vA5P
-43F4Gf82GjCg3259eE4z4EAP8N/0wIBrzDk1S8/PjSfgfO77AQG8pXzRfwk/QACt6H/gXo1eH9g/MOCE
-G+v6E/r1h/m/32zfgE8B6I/J0iUj9A8MCEDKx+/v6+2m898DA3og3/d43v49eM6fMh/p2e/BAy0Qfe3y
-84dngI2pAwX0D5xAC5pyf2BA/yCAq34e8oI/MGACpXz787uA1Xy4sVrg+T774sEDfCFAxznv4E/pC6i/
-L/sDA9pQz89tng953g94ng8fH3k/fACgC0aY/4EB0x3mE/A9Dmgf/40S4xI/3G1/YIAfI4f9fE/pAtkM
-6I234c9dG3z4mK8/9zC1AQY4EED6BwbIeL3433/e7u/BAyXQ0pnr3x5I4Inm3O/v4g/pL85B/A30Bw5M
-R4x5Bv/BgAMBfO3BfJ+eAnBgLpI9f+AInJIn/N3w95kXfDkP/O98iL81a+qO/X3Lff4wYMAA76X6/nL1
-9OACfK71dDfwv40XyP/A/U80G1pAY2sE94A/5Dgg+mBwA0RzXzBgQACpS9fXq5j2f/x/E+C06T5fE2BA
-D7ybfp9eG9AC6TQOO39I067n49m712b323g/d37vI7UjQ539/gIn6J929/e5Fz3u/f0kU9d6oQUDpAMb
-m/sDA3og/LhveA+88IIG7GvAgXsoBwwoQcp9A3w1g4u3/YABBvTAvL0HBrQA/x8/42x3X9x/89163q7/
-3UeAAn8d8YABAk/xO2x4A1o/MGCqfX3O1fSDAf3ggsL/qY4e941w/x95e2fAAyXYmD5gABy/E+c3vA6E
-A3uAf2DAeD6cW+s/OOB25fU3N337a4B/kIDvN0T/wIArsP34X8S96H9gwFQ8Ew5vAAYMMG3U5+oDDTj/
-8u4e0D/y/39gwFj99A8c8IcA17m4kAD3f7fX/327vQ3Qg6ZEfT5m+ocXmAM33f+x7e//e25A12L/s/z3
-jR/03c1EfxDA0yD9Awe0QArf9IeX89x3BfOAt9Tfh/2f9wUv1vQPDHBE2/95+S/y+8CBFjhUf2/4e37+
-0fH/+gA90A0eIADR6Tvw5b0FDl3Iww/w9X3+y/6AAYj+wX2D3u/13/tBf+A4p4/v1+iH7RkH7BfM24sB
-2T4wIADp1A+w+aL+m93wgguHw28q/S/bAQOkoAAtcACpX0iQhA8I20D/wIAxT19U3u/eS6e11mIggAIu
-yLd0v6/r3k2f2B+i/wM04IBA8p3s/T9vA43/wwO2iR3s2e/nAwOkw03sX2A88m4O9s8kIPhAfgpAOnXm
-eBf9sO/6Xf9mI4B/f8OABqA2t7I3q/e2s36/9f6/sXkggAKkw8C2012I495yYx/f91/3sD/sS8gX5eN3
-y3eL9z6263m9G5yP39zQ/8i6/T9/EwN0/l//m/S353f932e658y/27g5/gMD/Gg118S4v8f04cMB5pX5
-6037vQENKGCg/9p//3A/b2/d1x9wIA0oAep16+sCIn7x9I//vR60QAvww//qN/+fA/j/9nS85w9sD9AA
-9Y/CewP6e3d/P/9oM4a44f/m17+99/t/v3//610BByQQiC3A/X1kewAtcECu7fMv94//uN3i312m+u85
-+o7Gv+A58S+4s4k+8U9L2/f+/X4f2eT2vA3sZ/c//mZ4M/iH98x843mUv0d//eG2O7/v3e2//wIYoAUl
-eBf3AANm3jN63T3YBy4f/Pq3506L3+X8fR36G/qH+A2S/2324+1e/x28Bv445k9v0z4/4f7+/+i/uEw4
-d0s3A3B+4L/0v60/jP0/b2+3p+X0sAn6f/Pbf38X3q46P/62/s0xvwj7/X09G4/P4+1+/+/3f54D/YED
-LdBp3D/A87p+3D/+/W762+4P94EDd09r53c5/10/A2j+8O6G99/87v2n15/X+105v2u+9+L34B38AByQ
-4P0k6q81735p3D/Y/zIewH8x//xX8B3GgAPx4Tve8A/24ffwvwP28R5w/320+R78eGj89P43cGBf+3N9
-/0E3773/9G7664B3qP/5hPvd4d/v4yPvwffwLzjf93d9n2L8E3f3/p+fI/X4s6v44/E36PfhveK/37w9
-n8/3O/c38X2L3/24m7/e33d0/38//eE/2n/vv34/f2L/8E//q68/XU+3P/6Lfn/Efbx/v84A7v2BqVvC
-e/R/2A/40f6vX2N3E3q+/j68//2Dfl///mfeL6f/p2Sfxo//eP/5+wO74f8E6n97sX96e54f/PvdvB5p
-fvw+/p6mBvjXf/X/fEfxE3e81397//q/56T83+x2c2/2i/5v9j0/7/e39z30f4X/3d+48A2//0H1N2v7
-8T/q2++I+A+2e7r//0/EAtj7Nf9s813a3/u/v1v4f3t/e31s4z2P7fD7eA7uH/2b8x0/b/++NlS6f79u
-93S+n+f5fUfv8S+2v1v7f98x3i/8z0Cj/6f/BvXfrx53d++9jP8a+8/29A71j/8A/83O8/7E+x/A/p4G
-CugE8P+A88e/z//hvu0X839a8xH4+f7f8Xvf/30a/uM51H//p+7m74Hfx3A//3v9ffxO3fP/67e233zP
-2I/f+4a/Bf4C/2fA//z3v+sA57/8f9S9m/078J5+P7f215m//x7u2X/T99P+T3vf8C3+E+pX4j++p+/p
-+z7f3m68//vY9mO423s13e09dff/m4L+/d383m/i074a3v++O/mG55+O42/H/Xz/vT+D8d8Gv/39S/82
-X+L//b9e8N3i0+mB5q6nN4/fQ/eCfvxf32H/C/w992sS+L4X4x/mCfx/yN8C3v88Xz1N7D3j94D48/Tj
-LfwD1P8e/sH/4+8D3H9v/+mBq+m/fzf+/p9aP/0L+q/fB926v6H/D+D5/m3v0A38e8A9/sX9m6/O+5++
-67f7e914//uH/17/3yP+xH//0//57/XfD+oP3P+/qX9eI/j++Pz83eD5n34/+R/X74v/Rfr//4G/0O9O
-+r1O/f3/fQ3eN5z/fS7+f8P12D/eO374x38/Ltz/f+A3f+D9eG4f9+k///v5wvd/3X5v9P3256A/cOAH
+eJxN3U1y3DoOBuD1d3HVATr1D/WA3b/KTTreI3K8T8a8P98/x//Pz9//9kEDfND4QQ/qQch+fD4e
+f3qAAnzQeL5//9B0vvdC+aDx+Xk/fKCb2/v2QfmgI/f32gd9e6A3v3t/f2+aP/Dtg769v1//6G4/6Nt7
+v1/vG/7Qj//eN+sD/e3vD/vA/1PzB3x7f6828Ie/f2+/b/y/qfn/32f2f11+f/f3aO3p/uGvev1A15f6
+fX0Xm3l9mS/399/t/v3o+s+sD7T4x33P/p70T3y53S+/f7e/59sH3dy3x33Qe0Hj+/p+v++f/f493/1N
+mvhD/x+9L30Nvn4Dvdv7xT4/N6sPPs92o9fX69X2A+vN3m0/m09/o7Xvf839A293z/p6vf4aX//F63f1
+N+r9A+/j9ftHfeA33jfwvv5+/Xo9s4+/zL5/43/33z94+a90//mS+z4+m2++/7o2ff+2j//5/A3eN4/X
+b/G34Hrfv9Tf2/614PXe6GvjD4m/Uvf333X1P9rNf51//c29//vO622+mfeC/X/7r032m//3D9m04v6e
+1zL/TfaX4pvn632m3xI/z/p95u//+c7o/y038M+03nfg6z64v6e4j7+p5ffG/Nvf32i+/4uX+f/8H2O8
+mfl1qO+m92p8d3LffL3Nf9eL5/aN3jTebPPd78X/0pvm23y93c+f3994/fI8b56v999lS/rXm+erfX3j
++/S7vfn6e//6rX3X++2X7//bH2B+w7i/zX390j/Q/sS3T3x/7w+8/36z2+v7bL5/2X7130+2b3a//eB6
+c92+4z++qL5vH03f90+/qL/f1j3eN3+Pze81+M1d+9n21/D88N3c/uT7xTff4I/36aPvA9vX77m/Z//6
+N/0X8P1g23xX9P3sD7a+L9eXm+/vA2/uN3rf3//j/c329/7yP33rA3T/+pDvf3/1d38R/2W3+3rfp/P/
+3/u238d2f+fX//5G09evxR/+9v9u9/4d//H5+j7j+4jvf9m2p7/c/52+3/2f+zP1d1v/+/36+219vS+u
+/2qbf7qft1e3/O//m16/EfyI/eD6NfD4+U3Tvvv1qflx3f1fL9m+/lff7/M//qbbv8483z+4L9Xff6e5
+33N+76+/19//49pff9M/pD/+x3u++d3L9q//x/7339f4O8/34y+/w/TfH7/O11++9u3vH9sHfU1e//pG
+5+8vM5r34293Nl//E1e+XvPdr9/k/mXG6y+/x//267/j9d5k2/y5r/eA8f+2vv/490n87fH9O/7s2zeP
+//p1m+/H/168X79k//9sff1fX9e78/f7rI830//k3s/z/99j+e/1v33o1+/i7X3m+u12p7f/2mN/wOf/
+8//u2zf1j1x/3d53pX/f5fve/b/5X/O+3tA33f/6pvn3v9c/vv813w2f19vb1X77NfLp64v89z1p/i2u
+e7S2//j9m49vNf++E38n4p/t2xf2nff8Xv6f67vXl9pX+/qUft+5/k7r7941ve9S/P+p+D0fvvfe/v+p
+1381/+7u67/s/jfxq/4/eJvX+8z/3719nfn/a33v0ffP+T//Pq//1eN7//z5X+/E3/I13/z22m8z9/3b
+3x//62ve12Pxe3O8X1e8/1f63j+fvx+/8/d43X3T1yD8mH63b/7/I14Pvt52f4v2j+Yf+m/v+9qf61/7
+s+/O3mbf+L993S/N//T43S7/Uu4/v++Xf/+/12f53d/H7u003z+a3zT/Dfb4u8/j+/d32/d4b4//yXtf
+bN91+1+2/e+G+/fbf1//90e+/kL+/oHvx+b/+Zvv8+b7n2/z+/f7b2S+X5f/a7b/39XmX43X67q0v8a+
+1v63eL42+u27969837a/Xbf3I/r7I3O/vv0L2b5vvf9+u634Rvt3/e/1b+4/2+5t+Xf179c3T/9p/b9r
++zH4/Z+/Sfdxvf+e23/83eH//aLvv4fH2/eN/+3/O9e/vE//+oX8/8H0/kH1T+L/E94m4X90+z/+m/m+
+fv81vTf9A3v/yvd6+b+/3f769/6N22+S3x/6m5jv/y423+f8y2m+Xv1/5vG5Xv+2P9O64/744m+F//d+
+v/r++2d+z/fveO/E/+q9P3pvhvfbvXfD+P//+/d9/xP//4vf7e62vvqG+0v3f3e6vvI3aN+4+/uMv1/k
+93z1/f/a+/l1Nn3/z5/vY69d/sT03l76e8x/v+9/sL9++qI35p+/r94ffn99L+Jv3t/a/O//4f0d61v9
+35veE2wfuD7v/6N3a3yX974e+XpffXm+v+b2d4L4P91eX177/ffm29d89/4+/R/y/f+/9/Wj9w9u31e4
+/xPuv3i+//veo367X/6/f1sP/K/fO+e/u3/xvfB5L3S/e7x3e/m/uX198L//5fv762s78A2+y81eN6uX
++N9s4X8//p/+f/1H3m9N3w//eJ/55nv149/1ve1vT+bvhXf/43/+0vffX2p///35+l7vX2Xm89vH5/2T
++3f23iN6/+x3e3f3/3/rXv7//m33/2O9T6N93/O7u60X0f8Tfx+L3f1u7x939w1+3/P4+Xncf1+L1+/n
+fT154p/y/W963/f6fWff45fA22j143a3xvd9G/E/9/e4+S5+0//N3y3//X4e1v/5+/r+9n+H//35fQ/f
+3//6G/yA/+5/zdeL9x+/u//6E1/fvL7/+V8e4e+4/R+d/9v+e4v/d3a//9O/42/j17///fT1j942976w
+/yvU33f4fvMvxft2X193X7/5e3j3O/X7bH//I/v34e42a/7p++/dffx1evp/N/9+/x7/E343/1184++M
++/vF3fP2feL/8/3pvuN3ff1vE3mffvf+r64m+m//x7u2X/m3p+Lvv8f6r9S/+I///++f7e34O+I6M33P
+d38n/p/+9+3b2ff+eX+f6O9s9v2P3t/o69vxL274ff/eN/29L9c/0O+L/R+6f9m/C++vv4v4v//+/fv8
+8fX6jH41+/Ptf+0f8T1G/d193/I6m337M/G3/r/3y3vf3z//x/fe2+3L7S/+m+/v8939+P6H3Tf3TfB9
+9P42m/rfePvNvv7N7/Xf92x/t7v9R3r/+4v5e2/4fv+/x1v8b/T3Xp/mXzrf/x2uX/+xL4u//x3vX0f/
+M+3vf92X+H8R/63b/7e8P//X9/D+/36/N3q9iX8D4f46/f4/X3++X4O///4b+11/+zvev6bfb9n/5n5e
+b/+5L+1fvX+/5v458X1jff8n/qZ5X6ff32f91/m+vve4xPfC9y3v29H/j92/+d/p/+2+m3/v1S+33X2f
+++f5H81X6S//4X25fR2v+13ff03N/jG8D1f/8pvn338R//72X1+8p+/X2989eL5evS++3s63L9H9E3vf
+2/6//u4C+H23ft4ffS3ef+26r0m8/c70X/C5v8m3//D/2z4y4/s83vd0e73eN67f+3pG8+O4fvG2r8Xp
+//8b4f//d++75t/f4vf1x+/T3++X32d/2242/8/5eXv4H39745u1x/p35m///2/83XG96X04f/5+b34X
+zU//t++p2dfY79vM8/qA7m94f3l/y2vX12fxv6f//w+917T8Xk/8j+5ffM9mX/vF33v5f92n/D1Nn7N3
+36vXk/+T/W/X5v3//D5w/xrfZ13m//q3v76i3l+i+S91f0//S7bf/v8399eN//0/0L66/r2P/6f3vNnv
+A3ff5/T36d8f8Xk5///m9+X77r+m5o+396e///D3/7fX+wWv97fA+4bf//p70ePvS8x3l77vA/p39vf9
+A++v5t9p/bO4n7+Z/m/w5vfV+x/8+/T/+vfT/lX8f/k+//0///vj/v/v8X/fP3s2m/8X
 """
 
 def _build_lookup_set() -> set:
@@ -62,14 +66,13 @@ def _build_lookup_set() -> set:
     lookup = set()
     for code in raw_list:
         clean = code.lstrip('0')
-        lookup.add(code)           # '10010000'
-        lookup.add(clean)          # '10010000'
-        lookup.add(code[:4])       # '1001'
-        lookup.add(clean[:4])      # '1001'
+        lookup.add(code)
+        lookup.add(clean)
+        lookup.add(code[:4])
+        lookup.add(clean[:4])
     return lookup
 
 
-# Автоматическая загрузка полного реестра кодов в память при запуске программы
 GUARDED_LOOKUP_SET = _build_lookup_set()
 
 
@@ -81,18 +84,11 @@ def is_cargo_guarded(gng_code: str) -> bool:
     if not clean_input:
         return False
 
-    # 1. Точная проверка (на случай ввода 8-значного кода)
-    if clean_input in GUARDED_LOOKUP_SET:
-        return True
-
-    # 2. Дополнение нулями справа для коротких кодов (например, '1001' -> '10010000')
     padded_right = clean_input.ljust(8, '0')
-    if padded_right in GUARDED_LOOKUP_SET:
-        return True
-
-    # 3. Проверка по 4-значной товарной позиции
     prefix_4 = clean_input[:4]
-    if prefix_4 in GUARDED_LOOKUP_SET:
-        return True
 
-    return False
+    return (
+        clean_input in GUARDED_LOOKUP_SET
+        or padded_right in GUARDED_LOOKUP_SET
+        or prefix_4 in GUARDED_LOOKUP_SET
+    )
