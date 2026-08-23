13e9e98e8d7eac6efe04c8aa58eada805e2f0a68
Bazilio050 <yalik.g@gmail.com>
2026-08-23 12:38:54 +0400

    Delete engine.py

--- engine.py
+++
@@ -1,9 +0,0 @@
-# ==============================================================================
-# ПРОСЛОЙКА ДЛЯ СОВМЕСТИМОСТИ СО СТАРИМИ ТЕСТАМИ И APP.PY
-# ==============================================================================
-from core_engine.engine import calculate_freight_tariff
-
-# Алиас для поддержки старого имени функции из tests.py
-process_full_calculation = calculate_freight_tariff
-
-__all__ = ["calculate_freight_tariff", "process_full_calculation"]
