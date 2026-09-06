b7119ce98c3c1614febfb3c2dc3cfc98eaa9001f
Bazilio050 <yalik.g@gmail.com>
2026-09-06 12:23:57 +0400

    Update router.py

--- core/router.py
+++ core/router.py
@@ -17,6 +17,13 @@ class ShipmentType(Enum):
 
 
 @dataclass(frozen=True)
+class StationInfo:
+    code: str
+    canonical_name: str
+    is_border: bool
+
+
+@dataclass(frozen=True)
 class RouteResult:
     from_station: StationInfo
     to_station: StationInfo
