1a7b70a6004a2902928908445e31a54367ad769c
Bazilio050 <yalik.g@gmail.com>
2026-09-07 09:17:06 +0400

    Update calculator.py

--- core/calculator.py
+++ core/calculator.py
@@ -2,8 +2,8 @@
 
 from typing import Dict, Any, Optional
 from core.router import RailwayRouter
-from core.tables.table_3 import Table3Calculator
-from core.tables.table_4 import Table4Calculator
+from core.tables.table_3.calculator import Table3Calculator
+from core.tables.table_4.calculator import Table4Calculator
 from core.main_rules import apply_main_rules
 from data.translations import RULE_MESSAGES
 
