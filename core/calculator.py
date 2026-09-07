4712b4af18535bf842c8875df25d1a5b2c983551
Bazilio050 <yalik.g@gmail.com>
2026-09-07 13:26:15 +0400

    Update calculator.py

--- core/calculator.py
+++ core/calculator.py
@@ -33,8 +33,7 @@ class TariffCalculator:
         # 1. Расчет billable_weight (Таблица 1 / Минимальные нормы)
         weight_res = Table1Calculator.calculate_billable_weight(
             gng_code,
-            actual_weight,
-            wagon_type
+            actual_weight
         )
         billable_weight = weight_res["billable_weight"]
 
