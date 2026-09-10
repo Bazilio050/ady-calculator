13da345b9f98c6ecdf0b2552e5e5e528908e2ea1
Bazilio050 <yalik.g@gmail.com>
2026-09-10 09:40:23 +0400

    Update calculator.py

--- core/calculator.py
+++ core/calculator.py
@@ -142,7 +142,7 @@ class TariffCalculator:
             wagon_type=wagon_type_lower,
             from_canonical_name=from_canonical_name,
             to_canonical_name=to_canonical_name,
-            is_table_3=is_table_3_applicable and not (is_ref_wagon or is_tank_wagon),
+            is_table_3=is_table_3_applicable and not (is_ref_wagon or is_tank_wagon or is_empty_wagon),
             is_oil_product=is_oil_product,
             is_empty=is_empty_wagon,
             is_private_wagon=is_private_wagon
