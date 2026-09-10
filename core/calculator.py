eac057bc3eb3ae12fd14d05f15f28485cbf0a679
Bazilio050 <yalik.g@gmail.com>
2026-09-10 14:23:30 +0400

    Update calculator.py

--- core/calculator.py
+++ core/calculator.py
@@ -110,12 +110,12 @@ class TariffCalculator:
 
         if is_tank_wagon:
             billable_weight = 25.0
-        elif is_table_7 or is_inv_anv:
-            # Для Таблицы 7 и İNV/ANV берем фактический вес пользователя без округления по Таблице 1
-            billable_weight = float(actual_weight)
         elif is_auto_special and is_empty_wagon:
             # Для п. 3.3.2 в порожнем состоянии фиксируем вес: 5т для кузовов, 7т для автопоездов
             billable_weight = 5.0 if wagon_type_lower in ("auto_body", "detachable_body", "кузов") else 7.0
+        elif is_table_7 or is_inv_anv:
+            # Для Таблицы 7 и İNV/ANV берем фактический вес пользователя без округления по Таблице 1
+            billable_weight = float(actual_weight)
         else:
             weight_res = Table1Calculator.calculate_billable_weight(weight_after_min_norm)
             billable_weight = weight_res["calculated_weight"]
