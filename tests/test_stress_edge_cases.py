b37b31d28dd9468f9a1621d8ca906bda0aa85e26
Bazilio050 <yalik.g@gmail.com>
2026-09-12 21:16:26 +0400

    Update test_stress_edge_cases.py

--- tests/test_stress_edge_cases.py
+++ tests/test_stress_edge_cases.py
@@ -59,8 +59,8 @@ def test_complex_multi_rule_combination():
         wagon_type="covered",
         is_dangerous_cargo=True,
         is_passenger_train_composition=True,
-        is_attached_wagons=True,
-        attached_wagons_count=2,
+        is_coupled_wagons=True,
+        coupled_wagons_count=2,
         from_canonical_name=route_res.from_station.canonical_name,
         to_canonical_name=route_res.to_station.canonical_name,
         is_private_wagon=True
